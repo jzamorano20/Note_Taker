be2c0ae0bd7b432827a482a3d1ab66bf1b266aca
jzamorano20 <126306433+jzamorano20@users.noreply.github.com>
2023-04-07 14:44:29 -0400

    fixed something

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express =require('express');
-const apiRoutes = require('../Note_Taker/routes/apiRoutes');
-const htmlRoutes = require('../Note_Taker/routes/htmlroutes');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlroutes');
 const PORT = process.env.PORT || 3001;
 
 const app = express();
@@ -10,7 +10,7 @@ app.use(express.urlencoded({extended: false }));
 
 app.use(express.static('public'));
 
-app.use('./',apiRoutes);
-app.use('./', htmlRoutes);
+app.use('/api',apiRoutes);
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => console.log ('Listening on port %s', PORT));
